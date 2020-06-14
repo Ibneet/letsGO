2609b911822a174183294a30fc26193b4824f374
Ibneet <5ibneet@gmail.com>
2020-06-14 12:18:48 +0530

    .

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect('mongodb+srv://Ibneet:waheguru@letsgo-j6hql.mongodb.net/Journeys?retryWrites=true&w=majority')
+    .connect('mongodb+srv://Ibneet:waheguru@letsgo-j6hql.mongodb.net/LetsGo?retryWrites=true&w=majority')
     .then(() => {
         app.listen(5000);
     })

1abcaddc0e3a215b7ee6feab14393983af67fb48
Ibneet <5ibneet@gmail.com>
2020-06-28 16:35:23 +0530

    chat users retrieval done

--- controllers/userchat-controllers.js
+++ controllers/userchat-controllers.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 
 const HttpError = require('../models/http-error');
 const UserChat = require('../models/userchat');
+const User = require('../models/user');
 
 
 const addUserchat = async (req, res, next) => {
     const { from, to } = req.body;
 
+    const cid = from < to ? from + to : to + from;
+
     let userchats;
     try {
-        userchats = await UserChat.findOne({ from: from, to: to });
+        userchats = await UserChat.findOne({ chatID: cid });
     } catch (err) {
         const error = new HttpError(
             'Something went wrong, could not find the chat.',
@@ -22,7 +25,7 @@ const addUserchat = async (req, res, next) => {
         userchats = new UserChat({
             from,
             to,
-            chatID: from < to ? from + to : to + from,
+            chatID: cid,
         });
         try {
 
@@ -45,8 +48,54 @@ const addUserchat = async (req, res, next) => {
     console.log("userchats added");
 
     res.json({ userchat: userchats });
+}
 
+const getChatUsers = async (req, res, next) => {
+    const loggedInUser = req.user._id;
+    let userChats;
+    try{
+        userChats = await UserChat.find({
+            $or: [
+                { from: loggedInUser },
+                { to: loggedInUser }
+            ]
+        }
+        );
+    }catch(err){
+        const error = new HttpError(
+            'Something went wrong, could not retrieve the chats.',
+            500
+        );
+        return next(error);
+    }
     
+    if(!userChats || userChats.length === 0){
+        const error = new HttpError(
+            'No chats found.', 
+            404
+        );
+        return next(error);
+    }
+
+    var chatUsers = [];
+    var chat;
+    try{
+        for(var index in userChats){
+            chat = userChats[index].from == loggedInUser 
+            ? await User.findById({_id: userChats[index].to})
+            : await User.findById({_id: userChats[index].from})
+            chatUsers = chatUsers.concat(chat);
+        };
+    }catch(err){
+        const error = new HttpError(
+            'Something went wrong, could not find the chats.',
+            500
+        );
+        return next(error);
+    }
+
+    res.json({chatUsers: chatUsers});
 }
 
 exports.addUserchat = addUserchat;
+exports.getChatUsers = getChatUsers;
--- routes/userchat-routes.js
+++ routes/userchat-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const userchatControllers = require('../controllers/userchat-controllers');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/',userchatControllers.addUserchat);
+router.post('/', userchatControllers.addUserchat);
+router.get('/userChats', auth, userchatControllers.getChatUsers);
 
 module.exports = router;
